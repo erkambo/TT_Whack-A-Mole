1e6cd32944de9efba4f99f8a6366da1a86ca0cde
Erkam Boyacioglu <boyaciogluerkam@gmail.com>
2025-06-24 18:56:26 -0400

    Fixed errors

--- src/project.v
+++ src/project.v
@@ -120,7 +120,13 @@ module tt_um_whack_a_mole(
     input  wire        game_end,
     output wire [6:0]  seg,
     output wire        dp,
-    output wire [7:0]  led_score
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    output wire [7:0]  led_score,
+    input  wire       ena
 );
     wire [2:0] rand_seg;
     wire [2:0] segment_select;
@@ -155,6 +161,18 @@ module tt_um_whack_a_mole(
         .seg            (seg),
         .dp             (dp)
     );
+
+
+
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
+
 endmodule
 
 `default_nettype wire
